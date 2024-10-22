cb1427f70475f561847d6855df789a9632abad96
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2024-10-22 14:19:41 -0400

    share button functionality

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // activate strict mode
 'use strict';
 
-//store the share button element into a JS variable
+//store the share button into a variable
 const btnShare = document.querySelector('.btn__share');
-const contactSection = document.querySelector('.contact');
+
+//store the .content__info-wrap <div> in a variable
+const contentInfoContainerEl = document.querySelector('.content__info-wrap');
+//store the .content__social-links-wrap <div> in a variable
+const contentSocialLinksContainerEl = document.querySelector('.content__social-links-wrap');
+
+//When the Share button is clicked we should toggle the .display-toggle class for the Info Container and the SocialLinks Container
 
 btnShare.addEventListener('click', function () {
-  contactSection.style.display = 'none';
+  contentInfoContainerEl.classList.toggle('display-toggle');
+  contentSocialLinksContainerEl.classList.toggle('display-toggle');
 });
