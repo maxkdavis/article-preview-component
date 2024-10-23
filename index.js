16a323cd38d19fb556c579c038dfafa2ae6b38a4
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2024-10-23 13:31:08 -0400

    update share button functionality

--- index.js
+++ index.js
@@ -10,19 +10,21 @@ const contentSocialLinksContainerEl = document.querySelector('.content__social-l
 //store the .content__btn-wrap <div> in a variable
 const contentBtnContainerEl = document.querySelector('.content__btn-wrap');
 //store the .btn__share_svg <svg> in a variable
-// const btnShareSvgEl = document.querySelector('.btn__share-svg');
+const btnShareSvgEl = document.querySelector('.btn__share-svg');
 
 //When the Share button is clicked, toggle the .display-toggle class for the Info and SocialLinks Containers
 btnShareEl.addEventListener('click', function () {
+  // Control display appearance
   contentInfoContainerEl.classList.toggle('display-toggle');
   contentSocialLinksContainerEl.classList.toggle('display-toggle');
+  // Control button color
+  contentBtnContainerEl.classList.toggle('dark-background');
 
-  contentBtnContainerEl.classList.toggle('dark');
-  btnShareEl.classList.toggle('dark-btn');
-
-  // if (contentInfoContainerEl.classList.contains('display-toggle')) {
-  //   contentBtnContainerEl.style.backgroundColor = 'var(--dark-gray-blue)';
-  // } else {
-  //   contentBtnContainerEl.style.backgroundColor = 'white';
-  // }
+  if (contentBtnContainerEl.classList.contains('dark-background')) {
+    document.getElementById('path1').style.fill = 'white';
+    btnShareEl.style.backgroundColor = 'var(--dark-blue)';
+  } else {
+    document.getElementById('path1').style.fill = '#6E8098';
+    btnShareEl.style.backgroundColor = 'var(--light-gray-blue)';
+  }
 });
