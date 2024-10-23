edbd3bfd9693bbf87f01d39ff771aaf1ea10b8ff
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2024-10-23 11:49:22 -0400

    share button functionality and styling

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 'use strict';
 
 //store the share button into a variable
-const btnShare = document.querySelector('.btn__share');
-
+const btnShareEl = document.querySelector('.btn__share');
 //store the .content__info-wrap <div> in a variable
 const contentInfoContainerEl = document.querySelector('.content__info-wrap');
 //store the .content__social-links-wrap <div> in a variable
 const contentSocialLinksContainerEl = document.querySelector('.content__social-links-wrap');
 //store the .content__btn-wrap <div> in a variable
 const contentBtnContainerEl = document.querySelector('.content__btn-wrap');
-//store the .profile <section> in a variable
-const profileContainerEl = document.querySelector('.profile');
+//store the .btn__share_svg <svg> in a variable
+// const btnShareSvgEl = document.querySelector('.btn__share-svg');
 
-//When the Share button is clicked we should toggle the .display-toggle class for the Info Container and the SocialLinks Container
-btnShare.addEventListener('click', function () {
+//When the Share button is clicked, toggle the .display-toggle class for the Info and SocialLinks Containers
+btnShareEl.addEventListener('click', function () {
   contentInfoContainerEl.classList.toggle('display-toggle');
   contentSocialLinksContainerEl.classList.toggle('display-toggle');
-  // contentBtnContainerEl.classList.toggle('.dark');
-  // contentBtnContainerEl.style.backgroundColor = 'red';
-  // profileContainerEl.classList.toggle('.dark');
 
-  //if the btnContainer had a .dark class on then ADD background color. If it does not contain the classList then REMOVE background color.
+  contentBtnContainerEl.classList.toggle('dark');
+  btnShareEl.classList.toggle('dark-btn');
+
+  // if (contentInfoContainerEl.classList.contains('display-toggle')) {
+  //   contentBtnContainerEl.style.backgroundColor = 'var(--dark-gray-blue)';
+  // } else {
+  //   contentBtnContainerEl.style.backgroundColor = 'white';
+  // }
 });
