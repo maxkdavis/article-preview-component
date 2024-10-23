f516869fda7c59f43362ebb26308b2ca2115c985
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2024-10-23 08:00:35 -0400

    share button functionality

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ const btnShare = document.querySelector('.btn__share');
 const contentInfoContainerEl = document.querySelector('.content__info-wrap');
 //store the .content__social-links-wrap <div> in a variable
 const contentSocialLinksContainerEl = document.querySelector('.content__social-links-wrap');
+//store the .content__btn-wrap <div> in a variable
+const contentBtnContainerEl = document.querySelector('.content__btn-wrap');
+//store the .profile <section> in a variable
+const profileContainerEl = document.querySelector('.profile');
 
 //When the Share button is clicked we should toggle the .display-toggle class for the Info Container and the SocialLinks Container
-
 btnShare.addEventListener('click', function () {
   contentInfoContainerEl.classList.toggle('display-toggle');
   contentSocialLinksContainerEl.classList.toggle('display-toggle');
+  // contentBtnContainerEl.classList.toggle('.dark');
+  // contentBtnContainerEl.style.backgroundColor = 'red';
+  // profileContainerEl.classList.toggle('.dark');
+
+  //if the btnContainer had a .dark class on then ADD background color. If it does not contain the classList then REMOVE background color.
 });
